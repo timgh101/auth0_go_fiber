d2a74474abd8b0fe7a920fa6fc4c26aa5a8e9378
Tim <tim101@gmail.com>
2024-02-20 09:40:46 +0000

    added callback handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -38,3 +38,50 @@ func Login(auth *auth.Authenticator, store *session.Store) fiber.Handler {
 		return c.Redirect(auth.AuthCodeURL(state), http.StatusTemporaryRedirect)
 	}
 }
+
+func CallbackHandler(auth *auth.Authenticator, store *session.Store) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+
+		session, err := store.Get(c)
+		if err != nil {
+			fmt.Println("error in Login handler while getting session")
+			c.Status(http.StatusInternalServerError)
+			return c.SendString("error in session")
+		}
+
+		// Exchange an authorization code for a token.
+		token, err := auth.Exchange(c.Context(), c.Params("code"))
+		if err != nil {
+			fmt.Println("error in CallbackHandler while getting token")
+			c.Status(http.StatusInternalServerError)
+			return c.SendString("error")
+		}
+
+		idToken, err := auth.VerifyIDToken(c.Context(), token)
+		if err != nil {
+			fmt.Println("error in CallbackHandler failed to verify token")
+			c.Status(http.StatusInternalServerError)
+			return c.SendString("error")
+		}
+
+		var profile map[string]interface{}
+		if err := idToken.Claims(&profile); err != nil {
+			fmt.Println("error in CallbackHandler failed to put token into profile")
+			c.Status(http.StatusInternalServerError)
+			return c.SendString("error")
+		}
+
+		session.Set("access_token", token.AccessToken)
+		session.Set("profile", profile)
+		err = session.Save()
+		if err != nil {
+			fmt.Println("error in Login CallbackHandler while saving session")
+			c.Status(http.StatusInternalServerError)
+			return c.SendString("error")
+		}
+
+		// Redirect to logged in page.
+		return c.Redirect("/user", http.StatusTemporaryRedirect)
+		// ctx.Redirect(http.StatusTemporaryRedirect, "/user")
+	}
+}
